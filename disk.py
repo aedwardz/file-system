327ffa6e9aeca3a0484f776d078fdc43d2b79a0b
aedwardz <antonile@uci.edu>
2025-01-26 17:37:51 -0800

    Disk class initialization complete

--- disk.py
+++ disk.py
@@ -2,31 +2,68 @@ from fileDescriptor import FileDescriptor
 from math import ceil
 BLOCK_SIZE = 512
 class Disk:
-    def __init__(self, blocks:int, d:int):
-        self.d = d
-        self.k = ceil(d/32) + 1
+    def __init__(self, blocks:int):
+        self.k = ceil(192/32) + 1
         self.blocks = blocks
         self.disk = [0] * blocks
         self.initializeDisk()
 
+    def allocate_block(self):
+        """Find a free block in the bitmap and allocate it."""
+        # print(self.disk[0])
+        for i in range(len(self.disk[0])):
+
+            if self.disk[0][i] == 0:  # Free block
+                self.disk[0][i] = 1  # Mark as used
+                return i  # Return the bitmap index
+        raise ValueError("No free blocks available.")
+
+
+    def deallocate_block(self, block_index):
+        """Free a previously allocated block."""
+        if self.disk[0][block_index] == 1:  # Block is in use
+            self.disk[0][block_index] = 0  # Mark as free
+        else:
+            raise ValueError(f"Block {block_index} is already free.")
     def initializeDisk(self) -> None:
         """
         Initializes the disk split up into the bitMap, File Descriptors, and the block content
         """
-        self.disk[0] = [0] * self.blocks
+        self.disk[0] = [0] * 512
         for i in range(1, self.k):
             self.disk[i] = [FileDescriptor() for j in range(32)]
 
+        for i in range(self.k):
+            self.disk[0][i] = 1
+        self.create_directory()
 
 
 
 
+    def get_file_descriptor(self, descriptor_index):
+        """Retrieve a file descriptor by index."""
+        block_index, fd_index = descriptor_index
+        return self.disk[block_index][fd_index]
+
+    def create_directory(self):
+        """Create a new directory."""
+        # Find a free file descriptor block
+
+        fd = self.disk[1][0]
+          # Free descriptor
+        fd.size = 0
+        fd.is_directory = True
+        fd.blockPointers.append(self.allocate_block()) # Allocate a block for children metadata
+        block = fd.blockPointers[0]
+        self.disk[block] = [(0,0) for i in range(512//8)]
 
 
 
-if __name__ == "__main__":
-    #
-    d = Disk(100, 200)
-    for row in d.disk:
-        print(row)
+    def __getitem__(self, item):
+        return self.disk[item]
 
+    def __setitem__(self, key, value):
+        self.disk[key] = value
+
+    def __len__(self):
+        return len(self.disk)
--- tests/diskTest.py
+++ tests/diskTest.py
@@ -10,12 +10,11 @@ class MyTestCase(unittest.TestCase):
         Test that the Disk class initializes the attributes correctly.
         """
         blocks = 100
-        d = 64
-        disk = Disk(blocks, d)
-        self.assertEqual(disk.d, d)
+        disk = Disk(blocks)
         self.assertEqual(disk.blocks, blocks)
-        self.assertEqual(disk.k, ceil(d / 32) + 1)
+        self.assertEqual(disk.k, ceil(192/ 32) + 1)
         self.assertEqual(len(disk.disk), blocks)
+        print(disk[0])
 
     def test_initializeDisk(self):
         """
@@ -23,20 +22,30 @@ class MyTestCase(unittest.TestCase):
         """
         blocks = 10
         d = 64
-        disk = Disk(blocks, d)
+        disk = Disk(blocks)
 
         # Check bitmap initialization
         self.assertEqual(len(disk.disk[0]), 512)
-        self.assertTrue(all(bit == 0 for bit in disk.disk[0]))
 
         # Check file descriptors initialization
         for i in range(1, disk.k):
             self.assertEqual(len(disk.disk[i]), 32)
             self.assertTrue(all(isinstance(fd, FileDescriptor) for fd in disk.disk[i]))
 
+        #check directory block
+        self.assertEqual(disk[disk.k],[(0,0) for i in range(512//8)])
         # Check uninitialized blocks remain 0
-        for i in range(disk.k, blocks):
+        for i in range(disk.k+1, blocks):
             self.assertEqual(disk.disk[i], 0)
+
+    def testAllocate(self):
+        blocks = 100
+        disk = Disk(blocks)
+
+        self.assertEqual(disk.disk[0].count(1), 8)
+        disk.allocate_block()
+        self.assertEqual(disk.disk[0].count(1), 9)
+
 
 if __name__ == '__main__':
     unittest.main()

42aab0e09a143c86b97c36b1ae752fb535c92f2c
aedwardz <antonile@uci.edu>
2025-01-26 21:04:22 -0800

    Changed implementation of fd class. Now has a list of block indices

--- fileDescriptor.py
+++ fileDescriptor.py
@@ -1,3 +1,16 @@
 class FileDescriptor:
+    def __init__(self,  is_directory=False):
+        self.size = -1
+        self.blockPointers = []
+        self.isDirectory = is_directory
+
+    def __repr__(self):
+        if self.size == -1:
+            return 'Free'
+        else:
+            return f"FD size{self.size}"
+
+class DirectoryDescriptor:
     def __init__(self):
-        pass
+        self.name = None
+        self.index = None
